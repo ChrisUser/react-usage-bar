fec6bb1f5a981f9805bb75b3837d5ac8ee27ae6c
ChrisUser <cristiano-raimondi@tiscali.it>
2022-03-01 09:34:37 +0100

    Typescript bundle fix

--- src/UsageBarDark.stories.tsx
+++ src/UsageBarDark.stories.tsx
@@ -1,17 +1,17 @@
 import * as React from "react"
 import UsageBar from "./UsageBar"
 
-export default { 
-    title: "Usage Bar Dark",
-    component: UsageBar,
-    parameters: {}
-  }
+export default {
+  title: "Usage Bar Dark",
+  component: UsageBar,
+  parameters: {},
+}
 
 const items = [
   {
     name: "UI",
     value: 10,
-    color: "#E75D04",
+    color: "#E85D04",
   },
   {
     name: "Photos",
@@ -32,40 +32,40 @@ const items = [
 ]
 
 export const darkMode = () => (
-  <div style={{backgroundColor: '#212121'}}>
+  <div style={{ backgroundColor: "#212121" }}>
     <UsageBar darkMode items={items} total={100} />
   </div>
 )
 
 export const darkModeWithoutLabels = () => (
-    <div style={{backgroundColor: '#212121'}}>
-      <UsageBar removeLabels darkMode items={items} total={100} />
-    </div>
-  )
+  <div style={{ backgroundColor: "#212121" }}>
+    <UsageBar removeLabels darkMode items={items} total={100} />
+  </div>
+)
 
 export const darkModeWithPercentages = () => (
-    <div style={{backgroundColor: '#212121'}}>
-        <UsageBar showPercentage darkMode items={items} total={100} />
-    </div>
+  <div style={{ backgroundColor: "#212121" }}>
+    <UsageBar showPercentage darkMode items={items} total={100} />
+  </div>
 )
 
 export const darkModeCompact = () => (
-    <div style={{backgroundColor: '#212121'}}>
-        <UsageBar showPercentage compactLayout darkMode items={items} total={100} />
-    </div>
+  <div style={{ backgroundColor: "#212121" }}>
+    <UsageBar showPercentage compactLayout darkMode items={items} total={100} />
+  </div>
 )
 
 export const darkModeCompactWithoutLabels = () => (
-    <div style={{backgroundColor: '#212121'}}>
-        <UsageBar removeLabels compactLayout darkMode items={items} total={100} />
-    </div>
+  <div style={{ backgroundColor: "#212121" }}>
+    <UsageBar removeLabels compactLayout darkMode items={items} total={100} />
+  </div>
 )
 
 export const error = () => (
-    <>
-        <p>If sum of values exceeds total.</p>
-        <div style={{backgroundColor: '#212121'}}>
-            <UsageBar items={items} total={50} />
-        </div>
-    </>
+  <>
+    <p>If sum of values exceeds total.</p>
+    <div style={{ backgroundColor: "#212121" }}>
+      <UsageBar items={items} total={50} />
+    </div>
+  </>
 )
